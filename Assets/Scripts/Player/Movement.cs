0ce340e1b0067046300076e0f1879f9a547d8528
Nicolas <nicolas.migues97@gmail.com>
2025-09-05 22:40:45 -0300

    Normalized

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -11,13 +11,12 @@ public class Movement
     public void Move(Vector2 inputs)
     {
         var _dir = new Vector3(inputs.x, 0, inputs.y);
-        _dir.Normalize();
         if (_dir != Vector3.zero)
         {
             Quaternion rot = Quaternion.LookRotation(_dir,Vector3.up);
             _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, rot, 4.25f * Time.fixedDeltaTime));
         }
-        Vector3 _newPos = (_rb.position + _dir * _speed * Time.fixedDeltaTime);
+        Vector3 _newPos = (_rb.position + _dir.normalized * _speed * Time.fixedDeltaTime);
         _rb.MovePosition(_newPos);
     }
 }
